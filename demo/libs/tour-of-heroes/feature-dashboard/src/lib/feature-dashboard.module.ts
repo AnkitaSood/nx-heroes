e23c63127848c1576e40b99352b76ca4edf21ec2
asood <asood@octolabs.io>
2021-08-12 10:39:39 -0500

    chore: add card component

--- demo/libs/tour-of-heroes/feature-dashboard/src/lib/feature-dashboard.module.ts
+++ demo/libs/tour-of-heroes/feature-dashboard/src/lib/feature-dashboard.module.ts
@@ -4,18 +4,20 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DataAccessHeroesModule } from '@shared/data-access-heroes';
+import {CardModule} from "../../../../shared/ui-kit/src/lib/card/card.module";
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
 ]
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule,
-    DataAccessHeroesModule,
-    RouterModule.forChild(routes)
-  ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        DataAccessHeroesModule,
+        RouterModule.forChild(routes),
+        CardModule
+    ],
   declarations: [
     DashboardComponent,
     HeroSearchComponent
