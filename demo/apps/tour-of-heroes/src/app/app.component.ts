e4e160c70847d3ff96fc9ceea13429ee0281db76
asood <asood@octolabs.io>
2021-08-27 16:38:33 -0500

    feat: theming

--- demo/apps/tour-of-heroes/src/app/app.component.ts
+++ demo/apps/tour-of-heroes/src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  host: {class: 'light-theme'}, //adding light-theme as default to the app
 })
-export class AppComponent {}
+export class AppComponent {
+}
 
--- demo/libs/tour-of-heroes/feature-hero-details/src/lib/hero-detail/hero-detail.component.ts
+++ demo/libs/tour-of-heroes/feature-hero-details/src/lib/hero-detail/hero-detail.component.ts
@@ -10,7 +10,7 @@ import {Observable} from "rxjs";
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: [ './hero-detail.component.css' ]
+  styleUrls: [ './hero-detail.component.scss' ]
 })
 export class HeroDetailComponent {
   hero$: Observable<Hero> = this.route.paramMap.pipe(
