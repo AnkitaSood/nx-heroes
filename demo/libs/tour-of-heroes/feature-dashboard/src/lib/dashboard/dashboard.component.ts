8ce9390c931d61f32ff7f31baa191189d6e94216
Kyler Johnson <kjohnson@octolabs.io>
2021-09-04 15:08:10 -0400

    update dashboard to display fav heroes

--- demo/libs/tour-of-heroes/feature-dashboard/src/lib/dashboard/dashboard.component.ts
+++ demo/libs/tour-of-heroes/feature-dashboard/src/lib/dashboard/dashboard.component.ts
@@ -1,21 +1,20 @@
 import { Component } from '@angular/core';
 import { Hero } from '@shared/models';
 import { HeroService } from '@shared/data-access-heroes';
-import {Observable} from "rxjs";
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.scss' ]
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  heroes$: Observable<Hero[]> = this.heroService
+    .pluckStateProperty('heroes')
+    .pipe(map((heroes) => heroes.filter((hero) => hero.isFavHero)));
 
-  heroes$: Observable<Hero[]> = this.heroService.state$.pipe(
-    map(heroState => heroState?.heroes.slice(0, 4))
-  );
-
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.heroService.loadHeroes();
