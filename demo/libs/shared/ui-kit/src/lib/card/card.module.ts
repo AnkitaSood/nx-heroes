89e96ae17c3a1a11deb77c70d33b8fc6e6e51a97
asood <asood@octolabs.io>
2021-09-03 19:39:12 -0500

    feat: update card to display hero details

--- demo/libs/shared/ui-kit/src/lib/card/card.module.ts
+++ demo/libs/shared/ui-kit/src/lib/card/card.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CardComponent } from './card.component';
 import {MatCardModule} from "@angular/material/card";
 import {RouterModule} from "@angular/router";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
     declarations: [
@@ -11,10 +12,11 @@ import {RouterModule} from "@angular/router";
     exports: [
         CardComponent
     ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    MatCardModule
-  ]
+    imports: [
+        CommonModule,
+        RouterModule,
+        MatCardModule,
+        MatIconModule
+    ]
 })
 export class CardModule { }
