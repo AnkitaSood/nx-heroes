2fad4cb4ccc523043579e03b2c263961544aff0a
asood <asood@octolabs.io>
2021-09-05 00:11:48 -0500

    feat: storybook init

--- demo/libs/shared/ui-kit/src/lib/card/card.component.stories.ts
+++ demo/libs/shared/ui-kit/src/lib/card/card.component.stories.ts
@@ -1,8 +1,20 @@
 import {Story} from '@storybook/angular/types-6-0';
 
 import {CardComponent} from './card.component';
+import {Meta, moduleMetadata} from "@storybook/angular";
+
+export default {
+  title: 'Cards',
+  component: CardComponent,
+  decorators: [
+    moduleMetadata({
+      imports: []
+    })
+  ]
+} as Meta<CardComponent>;
 
 const Template: Story<CardComponent> = (args) => ({
+  component: CardComponent,
   props: args,
 })
 
---
+++ demo/libs/shared/ui-kit/src/lib/list/list.component.stories.ts
@@ -0,0 +1,22 @@
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { ListComponent } from './list.component';
+
+export default {
+  title: 'Lists',
+  component: ListComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [],
+    })
+  ],
+} as Meta<ListComponent>;
+
+const Template: Story<ListComponent> = (args: ListComponent) => ({
+  component: ListComponent,
+  props: args,
+});
+
+
+export const Primary = Template.bind({});
+Primary.args = {
+}
