5888fb2bcf920f71827e382d62f6d264af890b28
asood <asood@octolabs.io>
2021-09-07 19:37:34 -0500

    feat: flip card component storybook

--- demo/libs/shared/ui-kit/src/lib/list/flip-card.component.stories.ts
+++ demo/libs/shared/ui-kit/src/lib/list/flip-card.component.stories.ts
@@ -1,21 +1,31 @@
 import {moduleMetadata, Story, Meta} from '@storybook/angular';
 import {FlipCardComponent} from './flip-card.component';
+import {MatDividerModule} from "@angular/material/divider";
 
 export default {
-  title: 'FlipCard',
+  title: 'FlipCardComponent',
   component: FlipCardComponent,
   decorators: [
     moduleMetadata({
-      imports: [],
+      imports: [MatDividerModule],
     })
   ],
 } as Meta<FlipCardComponent>;
 
-const Template: Story<FlipCardComponent> = (args: FlipCardComponent) => ({
-  component: FlipCardComponent,
+export const BasicFlipCard: Story = (args) => ({
   props: args,
+  template: `<app-flip-card>
+                <ng-container title>${args.title}</ng-container>
+                <ng-container content-title>${args['content-title']}</ng-container>
+                <ng-container details>${args.details}</ng-container>
+                <ng-container back-content>${args['back-content']}</ng-container>
+                </app-flip-card>`,
 });
 
+BasicFlipCard.args = {
+  title: 'Fancy Card Title',
+  'content-title': 'Fancy Card Content Title',
+  details: "Fancy Card Details",
+  'back-content': "Fancy Card Back Side"
+}
 
-export const BasicFlipCard = Template.bind({});
-BasicFlipCard.args = {}
