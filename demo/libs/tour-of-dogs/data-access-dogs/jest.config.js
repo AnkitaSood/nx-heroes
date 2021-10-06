b9e45f73a4a9281da1b8ae1cbcec9025fdcbe94d
Kyler Johnson <kjohnson@octolabs.io>
2021-10-06 12:20:47 -0400

    add data-access-dogs lib

---
+++ demo/libs/tour-of-dogs/data-access-dogs/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'tour-of-dogs-data-access-dogs',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../../coverage/libs/tour-of-dogs/data-access-dogs',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
