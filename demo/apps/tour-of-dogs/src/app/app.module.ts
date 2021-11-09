ebe94c5b48027c5f4d8b255d245e92b936f93e62
Kyler Johnson <kylerjohnsondev@gmail.com>
2021-11-09 12:02:53 -0500

    fix lint for tour of dogs

--- demo/apps/tour-of-dogs/src/app/app.module.ts
+++ demo/apps/tour-of-dogs/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { DataAccessDogsModule } from '@tod/data-access-dogs';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
-    DataAccessDogsModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
