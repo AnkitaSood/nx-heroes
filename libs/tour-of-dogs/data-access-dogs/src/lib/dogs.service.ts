44489ffd5979a4c7a7bc0c94e3ddc48ee39c0043
Kyler Johnson <kylerjohnsondev@gmail.com>
2022-01-08 17:31:50 -0500

    adding service methods to get breeds, get images by breed id, and set/delete/get favorite images

--- libs/tour-of-dogs/data-access-dogs/src/lib/dogs.service.ts
+++ libs/tour-of-dogs/data-access-dogs/src/lib/dogs.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { StateService } from '@shared/data-access-state-base';
-import { DogBreed, TourOfDogsConfig } from '@tod/tod-models';
+import { AddToFavoritesResponse, DogApiOrder, DogBreed, FavoriteImage, ImageSearchResponse, TourOfDogsConfig } from '@tod/tod-models';
 import { Observable } from 'rxjs';
 
 interface DogsState {
@@ -24,13 +25,57 @@ export class DogsService extends StateService<DogsState> {
     super();
   }
 
-  getDogBreeds(): Observable<DogBreed[]> {
-    return this.httpClient.get<DogBreed[]>(
-      `${this.config.baseUrl}/v1/breeds`,
-      { headers: this.headers }
-    )
+  getDogBreeds(page: number, limit: number, order: DogApiOrder): Observable<DogBreed[]> {
+    const params = new HttpParams()
+      .set('limit', limit)
+      .set('page', page)
+      .set('order', order)
+    return this.httpClient.get<DogBreed[]>(`${this.config.baseUrl}/v1/breeds`, {
+      headers: this.headers,
+      params: params
+    })
   }
 
+  getImagesByBreedId(page: number, limit: number, order: DogApiOrder, breedId: number): Observable<ImageSearchResponse> {
+    const params = new HttpParams()
+      .set('page', page)
+      .set('limit', limit)
+      .set('order', order)
+      .set('breed_id', breedId)
 
+    return this.httpClient.get<ImageSearchResponse>(`${this.config.baseUrl}/v1/images/search`, {
+      headers: this.headers,
+      params
+    })
+  }
+
+  getFavoriteImages(subId: string, page: number, limit: number): Observable<FavoriteImage[]> {
+    const params = new HttpParams()
+      .set('page', page)
+      .set('limit', limit)
+      .set('sub_id', subId)
+
+    return this.httpClient.get<FavoriteImage[]>(`${this.config.baseUrl}/v1/favourites`, {
+      headers: this.headers,
+      params
+    })
+  }
+
+  addImageToFavorites(subId: string, imageId: string): Observable<AddToFavoritesResponse> {
+    const body = {
+      image_id: imageId,
+      sub_id: subId
+    };
+
+    return this.httpClient.post<AddToFavoritesResponse>(`${this.config.baseUrl}/v1/favourites`, body, {
+      headers: this.headers
+    })
+  }
+
+  deleteImageFromFavorites(favoriteId: string | number): Observable<{ message: string }> {
+    return this.httpClient.delete<{ message: string }>(`${this.config.baseUrl}/v1/favourites/${favoriteId.toString()}`, {
+      headers: this.headers
+    })
+  }
 
 }
--- libs/tour-of-dogs/tod-models/src/lib/api-types.ts
+++ libs/tour-of-dogs/tod-models/src/lib/api-types.ts
@@ -3,7 +3,7 @@ export interface DogBreed {
   experimental: number;
   hairless: number;
   hypoallergenic: number;
-  id: string;
+  id: number;
   life_span: string;
   name: string;
   natural: number;
@@ -16,4 +16,56 @@ export interface DogBreed {
   temperament: string;
   weight_imperial: string;
   wikipedia_url: string;
+}
+
+export type DogApiOrder = 'RANDOM' | 'ASC' | 'DESC';
+
+export interface Height {
+  imperial: string;
+  metric: string;
+}
+
+export interface Weight {
+  imperial: string;
+  metric: string;
+}
+
+export interface Breed {
+  bred_for: string;
+  breed_group: string;
+  height: Height;
+  id: number;
+  life_span: string;
+  name: string;
+  origin: string;
+  reference_image_id: string;
+  temperament: string;
+  weight: Weight;
+}
+
+export interface ImageSearchResponse {
+  breeds: Breed[];
+  height: number;
+  id: string;
+  url: string;
+  width: number;
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
+
+export interface FavoriteImage {
+  id: number;
+  user_id: string;
+  image_id: string;
+  sub_id: string;
+  created_at: Date;
+  image: Image;
+}
+
+export interface AddToFavoritesResponse {
+  message: string;
+  id: string | number; // favoriteId
 }
