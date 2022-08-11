7324fcb0559649515567571dc6a63cd52107cffc
ankita <asood@octolabs.io>
2022-08-11 15:42:00 -0500

    feat: toggle dashboard layout

--- libs/tour-of-heroes/feature-dashboard/src/lib/feature-dashboard.module.ts
+++ libs/tour-of-heroes/feature-dashboard/src/lib/feature-dashboard.module.ts
@@ -4,17 +4,21 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroCardModule } from '@toh/ui-kit-heroes';
 import { DataAccessHeroesModule } from '@toh/data-access-heroes';
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatIconModule} from "@angular/material/icon";
 
 const routes: Routes = [{ path: '', component: DashboardComponent }];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule,
-    RouterModule.forChild(routes),
-    HeroCardModule,
-    DataAccessHeroesModule.forRoot(),
-  ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        RouterModule.forChild(routes),
+        HeroCardModule,
+        DataAccessHeroesModule.forRoot(),
+        MatButtonToggleModule,
+        MatIconModule,
+    ],
   declarations: [DashboardComponent],
   exports: [DashboardComponent],
 })
